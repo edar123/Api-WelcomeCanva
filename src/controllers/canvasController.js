71bc4a51efbc24d166af85ead658f24ec89572fd
Adfay-Inc <adfayinc@gmail.com>
2024-07-31 12:28:01 -0600

    No unnecessary console to delete

--- src/controllers/canvasController.js
+++ src/controllers/canvasController.js
@@ -1,17 +1,16 @@
 const { createCanvas, loadImage, registerFont } = require('canvas');
 const path = require('path');
 
-// Registrar la fuente Impact usando la ruta absoluta
 const fontImpactPath = path.resolve(__dirname, '..', 'assets', 'fonts', 'Impact-Regular.ttf');
 
 console.log('Registrando fuente desde:', fontImpactPath);
 registerFont(fontImpactPath, { family: 'Impact' });
 
 exports.generateWelcomeImage = async (req, res) => {
-  // Imprimir todos los parámetros recibidos para depuración
+  // Imprimir todos los Parámetros
   console.log('Query Parameters:', req.query);
 
-  // Extraer y validar parámetros, asegurándose de que están en mayúsculas
+  // Extraer y Validar Parámetros
   const username = (req.query.username || 'Usuario').toUpperCase();
   const avatarUrl = req.query.avatarUrl || '';
   const backgroundUrl = req.query.backgroundUrl || path.join(__dirname, '..', 'assets', 'default-bg.png');
@@ -38,8 +37,7 @@ exports.generateWelcomeImage = async (req, res) => {
   const canvas = createCanvas(width, height);
   const context = canvas.getContext('2d');
 
-  // Verificar las fuentes registradas
-  console.log(context.font); // Debería imprimir "10px sans-serif" por defecto
+  console.log(context.font); 
 
   // Cargar fondo
   try {
@@ -52,7 +50,7 @@ exports.generateWelcomeImage = async (req, res) => {
     context.fillRect(0, 0, width, height);
   }
 
-  // Cargar y dibujar logo en la esquina superior derecha
+  // logo en la esquina superior derecha
   try {
     const logo = await loadImage(logoUrl);
     const logoWidth = 200; // Ancho del logo
@@ -62,7 +60,7 @@ exports.generateWelcomeImage = async (req, res) => {
     console.error('Error al cargar el logo:', error);
   }
 
-  // Cargar y dibujar avatar
+  // Avatar
   let avatar;
   if (avatarUrl) {
     try {
@@ -77,7 +75,7 @@ exports.generateWelcomeImage = async (req, res) => {
     avatar = await loadImage(defaultAvatarPath);
   }
 
-  // Dibujar borde del avatar (ajustado para un tamaño más grande)
+  // Dibujar Borde del Avatar
   const avatarRadius = 260; // Tamaño del radio del avatar
   context.save();
   context.beginPath();
@@ -88,7 +86,7 @@ exports.generateWelcomeImage = async (req, res) => {
   context.fill();
   context.restore();
 
-  // Dibujar avatar (ajustado para un tamaño más grande)
+  // Dibujar Avatar 
   context.save();
   context.beginPath();
   context.arc(960, 400, avatarRadius, 0, Math.PI * 2, true);
@@ -97,7 +95,7 @@ exports.generateWelcomeImage = async (req, res) => {
   context.drawImage(avatar, 960 - avatarRadius, 400 - avatarRadius, avatarRadius * 2, avatarRadius * 2);
   context.restore();
 
-  // Función para centrar texto
+  // Centrar texto
   const drawCenteredText = (text, y, fontSize, color, weight = 'normal') => {
     context.fillStyle = color;
     context.font = `${weight} ${fontSize}px Impact`;
@@ -106,16 +104,16 @@ exports.generateWelcomeImage = async (req, res) => {
     context.fillText(text, (width - textWidth) / 2, y);
   };
 
-  // Texto de bienvenida con fuente más gruesa
+  // Texto de Bienvenida
   drawCenteredText(welcomeText, 780, 100, textColor, 'bold');
 
-  // Nombre de usuario
+  // Texto Nombre
   drawCenteredText(username, 870, 70, textColor, 'bold');
 
-  // Texto personalizado
+  // Texto Perzonalizado
   drawCenteredText(customText, 950, 60, borderColor, 'bold');
 
-  // Convertir canvas a imagen y enviar como respuesta
+  // Canvas a imagen y Enviar
   const buffer = canvas.toBuffer('image/png');
   res.type('image/png');
   res.send(buffer);
